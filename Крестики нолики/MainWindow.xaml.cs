f465bad36b89b95efb6ee978c6fb809513938705
marta01h <isip_m.a.sargsyan@mpt.ru>
2023-02-10 21:33:08 +0300

    Тик так на часах

---
+++ Крестики нолики/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+namespace TicTacToe
+{
+    public partial class MainWindow
+{
+        int turn;
+        int i, j;
+
+        public MainWindow()
+: base()
+{
+            this.InitializeComponent();
+}
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+{
+turn = 1;
+}
+
+        private void win(string btnContent)
+{
+            if ((Button1.Content == btnContent & Button2.Content == btnContent &
+Button3.Content == btnContent)
+| (Button1.Content == btnContent & Button4.Content == btnContent &
+Button7.Content == btnContent)
+| (Button1.Content == btnContent & Button5.Content == btnContent &
+Button9.Content == btnContent)
+| (Button2.Content == btnContent & Button5.Content == btnContent &
+Button8.Content == btnContent)
+| (Button3.Content == btnContent & Button6.Content == btnContent &
+Button9.Content == btnContent)
+| (Button4.Content == btnContent & Button5.Content == btnContent &
+Button6.Content == btnContent)
+| (Button7.Content == btnContent & Button8.Content == btnContent &
+Button9.Content == btnContent)
+| (Button3.Content == btnContent & Button5.Content == btnContent &
+Button7.Content == btnContent))
+{
+                if (btnContent == "O")
+{
+ 
+                    MessageBox.Show("Победил игрок 0");
+Label1.Content = ++i;
+}
+                else if (btnContent == "X")
+{
+                    MessageBox.Show("Победил игрок Х");
+Label2.Content = ++j;
+}
+disablebuttons();
+}
+
+            else
+{
+                foreach (Button btn in wrapPanel1.Children)
+{
+                    if (btn.IsEnabled == true)
+                        return;
+}
+                MessageBox.Show("НИЧЬЯ");
+}
+}
+        private void disablebuttons()
+{
+            foreach (Button btn in wrapPanel1.Children)
+{
+btn.IsEnabled = false;
+}
+}
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (turn == 1)
+            {
+                btn.Content = "O";
+                Label0.Content = "X";
+            }
+            else
+            {
+                btn.Content = "X";
+                Label0.Content = "O";
+            }
+            btn.IsEnabled = false;
+            win(btn.Content.ToString());
+            turn += 1;
+            if (turn > 2)
+                turn = 1;
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Button btn in wrapPanel1.Children)
+            {
+                btn.Content = "";
+                btn.IsEnabled = true;
+
+            }
+
+        }       
+
+    }
+}
